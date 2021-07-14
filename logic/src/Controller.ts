29b8443e5ae80a661bfbc78829019f13a5fe4431
enrico-dgr <elcidwar@gmail.com>
2021-07-14 23:32:19 +0200

    structured controller functions

--- logic/src/Controller.ts
+++ logic/src/Controller.ts
@@ -1,8 +1,9 @@
-import * as A from 'fp-ts/Arr';
+import { identity, pipe } from 'fp-ts/lib/function';
+import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
-import { Puppeteer as P, utils as LP_utils, WebDeps as WD, WebProgram as WP } from 'launch-page';
+import { Puppeteer as P, utils as LP_utils } from 'launch-page';
 
-import { getProgram, Program } from './Program';
+import { Options, Program } from './Program';
 
 // ------------------------------------
 // Models
@@ -10,29 +11,96 @@ import { getProgram, Program } from './Program';
 /**
  *
  */
-export interface Options<ProgramOptions> {
-  programOptions: ProgramOptions;
-  launchOptions: P.LaunchOptions;
-}
+type SetProgram = (
+  user: string,
+  nameOfProgram: string
+) => O.Option<Program<any, any>>;
 /**
- * 
+ *
  */
-export interface Controller {
-  launchProgram: <A>(user: string, program:string) => TE.TaskEither<Error,A>,
-  endProgram: (user: string, nameOfProgram:string) => TE.TaskEither<Error,void>,
-  getOptions: (user: string, nameOfProgram:string) => <ProgramOptions>(options:Options<ProgramOptions>) => TE.TaskEither<Error,void>
-  setOptions: (user: string, nameOfProgram:string) => <ProgramOptions>(options:Options<ProgramOptions>) => TE.TaskEither<Error,void>
+type GetProgram = (
+  user: string,
+  nameOfProgram: string
+) => O.Option<Program<any, any>>;
+/**
+ *
+ */
+type GetOptions = <ProgramOptions>(
+  user: string,
+  nameOfProgram: string
+) => TE.TaskEither<Error, O.Option<Options<ProgramOptions>>>;
+/**
+ *
+ */
+type SetOptions = (
+  user: string,
+  nameOfProgram: string
+) => <ProgramOptions>(
+  options: Options<ProgramOptions>
+) => TE.TaskEither<Error, void>;
+/**
+ *
+ */
+export interface ControllerOfPrograms {
+  launchProgram: <A>(user: string, program: string) => TE.TaskEither<Error, A>;
+  endProgram: (
+    user: string,
+    nameOfProgram: string
+  ) => TE.TaskEither<Error, void>;
+  getOptions: GetOptions;
+  setOptions: SetOptions;
 }
 // ------------------------------------
 // Constructors
 // ------------------------------------
 /**
- * 
+ *
  */
-const getLaunchProgram = (programs:Program<any,any>[]) => 
-  (user: string, nameOfProgram:string)=>
-  pipe()
+const getLaunchProgram = (setProgram: SetProgram) => (
+  getOptions: GetOptions
+) => <A>(user: string, nameOfProgram: string): TE.TaskEither<Error, A> =>
+  pipe(
+    setProgram(user, nameOfProgram),
+    O.match(
+      () => TE.left(new Error(`Program not found.`)),
+      (program) => TE.right(program)
+    ),
+    TE.chain((program) =>
+      pipe(
+        getOptions(user, program.name),
+        TE.map(O.match(() => program.defaultOptions, identity)),
+        TE.chain(({ programOptions, launchOptions }) =>
+          pipe(
+            P.launchPage(launchOptions),
+            LP_utils.startFrom<A>(program.self(programOptions))
+          )
+        )
+      )
+    )
+  );
 /**
  *
  */
-export const getController = (programs:Program<any,any>[]) => 
+const getEndProgram = (getProgram: GetProgram) => (
+  user: string,
+  nameOfProgram: string
+): TE.TaskEither<Error, void> =>
+  pipe(
+    getProgram(user, nameOfProgram),
+    O.match(
+      () => TE.left(new Error(`Program is not running.`)),
+      (program) => program.end()
+    )
+  );
+/**
+ *
+ */
+export const buildController = (getProgram: GetProgram) => (
+  getOptions: GetOptions,
+  setOptions: SetOptions
+): ControllerOfPrograms => ({
+  launchProgram: getLaunchProgram(getProgram)(getOptions),
+  endProgram: getEndProgram(getProgram),
+  getOptions,
+  setOptions,
+});
--- logic/src/Program.ts
+++ logic/src/Program.ts
@@ -1,16 +1,27 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as TE from 'fp-ts/TaskEither';
-import { WebDeps as WD, WebProgram as WP } from 'launch-page';
+import { Puppeteer as P, WebDeps as WD, WebProgram as WP } from 'launch-page';
 
 // ------------------------------------
 // Models
 // ------------------------------------
+/**
+ *
+ */
+/**
+ *
+ */
+export interface Options<ProgramOptions> {
+  programOptions: ProgramOptions;
+  launchOptions: P.LaunchOptions;
+}
 /**
  * It is not intended to construct instances on your own.
  * Use `getProgram` instead.
  */
 export interface Program<ProgramOptions, B> {
   name: string;
+  defaultOptions: Options<ProgramOptions>;
   self: (options: ProgramOptions) => WP.WebProgram<B>;
   end: () => TE.TaskEither<Error, void>;
 }
@@ -33,11 +44,10 @@ const closeBrowserAtEnd = <A>(program: WP.WebProgram<A>) =>
 /**
  * Get Program
  */
-export const getProgram = <ProgramOptions, B>(
+export const buildProgram = <ProgramOptions, B>(
   program: Program<ProgramOptions, B>
 ): Program<ProgramOptions, B> => ({
-  name: program.name,
+  ...program,
   self: (options: ProgramOptions) =>
     pipe(options, program.self, closeBrowserAtEnd),
-  end: program.end,
 });
