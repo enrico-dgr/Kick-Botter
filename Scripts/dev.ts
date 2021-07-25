b38265ea243c364abe10d0771b01ae100bf1402e
enrico-dgr <elcidwar@gmail.com>
2021-07-25 21:39:09 +0200

    fixed build-dir creation

--- Scripts/dev.ts
+++ Scripts/dev.ts
@@ -5,6 +5,7 @@ import rimrafShellPaths from './rimrafShellPaths';
 namespace CONSTANTS {
   export const pathsToCopy: string[] = ["../index.html", "../package.json"];
   export const emptyDirs: string[] = [
+    "./KickBotter",
     "./KickBotter/Programs",
     "./KickBotter/Programs/local",
   ];
