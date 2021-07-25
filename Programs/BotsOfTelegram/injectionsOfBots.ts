0f826a08b4fe3f29998cce06341a05789622c43b
enrico-dgr <elcidwar@gmail.com>
2021-07-25 22:22:47 +0200

    removed old file

--- Programs/BotsOfTelegram/injectionsOfBots.ts
+++
@@ -1,9 +0,0 @@
-import { injectBot, Options } from './botsOfTelegram';
-import { socialgift } from './Socialgift/deps';
-import { socialmoney } from './Socialmoney/deps';
-
-const bot = injectBot((language) => ({
-  Socialgift: socialgift(language),
-  SocialMoney: socialmoney(language),
-}));
-export { bot, Options };
